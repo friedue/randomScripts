e8276f03f0b54866076cfba9376202083080b654
Friederike Duendar <frd2007@med.cornell.edu>
2016-02-05 13:20:31 -0500

    put trimming of sequence into the proper function

--- ABC/bam_to_meCount.py
+++ ABC/bam_to_meCount.py
@@ -12,26 +12,21 @@ import gzip
 
 
 def get_args():
-    parser=argparse.ArgumentParser(description='Split suspender BAM files')
+    parser=argparse.ArgumentParser(description='Count numbers of methylated and unmethylated Cs per read')
     parser.add_argument('--BAMfile', '-in', type=str, required=True, help="BAM file")
     parser.add_argument('--outfile', '-o', type=str, required=True, help = 'Prefix for the output file of counts per reads')
     parser.add_argument('--trimStart', '-strim', type=int, required=False, default = 0, help = 'Number indicating how many bp should be ignored at the 5 prime end of the read.')
     parser.add_argument('--trimEnd', '-etrim', type=int, required=False, default = 0, help = 'Number indicating how many bp should be ignored at the 3 prime end of the read.')
+    parser.add_argument('--CpGonly', action='store_true', default=False, help = 'If set, only cytosines in CpG contexts are taken into account.')
     
     args=parser.parse_args()
     return args
 
-def get_read_seq(Read):
-    '''returns the sequence part of Bismark's XM tag'''
-    Seq = [item for item in Read.tags if item[0] == 'XM'][0][1]
+def get_read_seq(Read, strim, etrim):
+    '''returns the sequence part of Bismark's XM tag; if necessary,
+    the sequence is trimmed'''
     
-    return Seq
-
-
-
-def get_mecounts(Read_seq, strim, etrim):
-    '''counts the (un)methylated residues in a read'''
-    Counts = [0,0,0]
+    Seq = [item for item in Read.tags if item[0] == 'XM'][0][1]
     
     beg = 0 + strim
     if etrim > 0:
@@ -39,18 +34,33 @@ def get_mecounts(Read_seq, strim, etrim):
     else:
         end = None
         
-    Read = Read_seq[beg:end]
-   
-    for bp in Read:
-        if bp.isupper():
-            Counts[0] += 1 # methylated Cs
-        if bp.islower():
-            Counts[1] += 1 # unmethylated Cs
-        elif bp == ".": 
-            Counts[2] += 1 # not a C
+    return Seq[beg:end]
+
+
+
+def get_mecounts(Read_seq, CpG_only):
+    '''counts the (un)methylated residues in a read'''
+    Counts = [0,0,0]
     
-    if sum(Counts) != len(Read):
-            raise NameError("Individual counts (%d) do not match the length of the (trimmed) read sequence (%d)" % (sum(Counts), len(Read)))
+    if not CpG_only:
+        for bp in Read_seq:
+            if bp.isupper():
+                Counts[0] += 1 # methylated Cs
+            if bp.islower():
+                Counts[1] += 1 # unmethylated Cs
+            elif bp == ".": 
+                Counts[2] += 1 # not a C
+    else:
+        for bp in Read_seq:
+            if bp == "Z":
+                Counts[0] += 1
+            if bp == "z":
+                Counts[1] += 1
+            if bp == "." or bp.isupper() or bp.islower:
+                Counts[2] += 1
+    
+    if sum(Counts) != len(Read_seq):
+            raise NameError("Individual counts (%d) do not match the length of the (trimmed) read sequence (%d)" % (sum(Counts), len(Read_seq)))
     
     return Counts
 
@@ -73,8 +83,8 @@ def main():
     fo = gzip.open(args.outfile + '.tab.gz', 'wb')
         
     for DNAread in infile:
-        me_seq = get_read_seq(DNAread)
-        c = get_mecounts(me_seq, args.trimStart, args.trimEnd)
+        me_seq = get_read_seq(DNAread, args.trimStart, args.trimEnd)
+        c = get_mecounts(me_seq, CpG_only = args.CpGonly)
         me_percent = get_mepercent(c)
 
         fo.write('{}\t{}\t{}\t{}\t{}\n'.format(DNAread.query_name, me_percent, c[0], c[1], sum(c)))
